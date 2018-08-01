5182f7aee61af088d84aea379cad07699d2ed905
Aditya REddy <aditya.reddy@hotmail.co.nz>
2018-08-01 20:03:17 +1200

    latest

--- js/script.js
+++ js/script.js
@@ -4,9 +4,15 @@ $.ajax({
     url: "https://api.geonet.org.nz/news/geonet" ,
     dataType: "json" ,
     success:function(data){
+
+      // var data = google.visualization.arrayToDataTable([
+      //      ['PhysicalQuantity', 'Value'],
+      //      ['Temperature', data.with[0].content.Temperature],
+      //      ['Humidity', data.with[0].content.Humidity]
+      //    ]);
       console.log(data.feed);
       for (var i = 0; i < data.feed.length; i++) {
-        console.log(data.feed[i].title);
+        console.log(data.feed[i]);
       }
     },
     error:function(error){
