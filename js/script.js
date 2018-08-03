8ec975a6df5e3160b5d7da8b753aa87d54bc8ad5
Aditya REddy <aditya.reddy@hotmail.co.nz>
2018-08-04 11:59:13 +1200

    latest

--- js/script.js
+++ js/script.js
@@ -5,14 +5,23 @@ $.ajax({
     dataType: "json" ,
     success:function(data){
 
-      // var data = google.visualization.arrayToDataTable([
-      //      ['PhysicalQuantity', 'Value'],
-      //      ['Temperature', data.with[0].content.Temperature],
-      //      ['Humidity', data.with[0].content.Humidity]
-      //    ]);
       console.log(data.feed);
+      var tb = document.getElementById('disp');
       for (var i = 0; i < data.feed.length; i++) {
-        console.log(data.feed[i]);
+          var tr = document.createElement('tr');
+          var th1 = document.createElement('th');
+          var th2 = document.createElement('th');
+          var th3 = document.createElement('th');
+          var text = document.createTextNode(data.feed[i].title);
+          var text1 = document.createTextNode(data.feed[i].link);
+          var text2 = document.createTextNode(data.feed[i].published);
+          th1.appendChild(text);
+          th2.appendChild(text1);
+          th3.appendChild(text2);
+          tr.appendChild(th1);
+          tr.appendChild(th2);
+          tr.appendChild(th3);
+          tb.appendChild(tr);
       }
     },
     error:function(error){
